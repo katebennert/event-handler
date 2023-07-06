ec541afc2776107e8991d89fae3b38ad58a53862
Kate Bennert <kate.bennert@gmail.com>
2023-07-06 13:41:24 -0400

    Create sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :venues
+  resources :venues 
   resources :comments
   resources :events
-  resources :planners
+  #resources :planners
+  #need route to planners because i need the extra planner info (unless i get nested from somehwere)
   resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
